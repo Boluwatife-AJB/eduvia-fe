d976cae017efce88fd477a487b10dc86f6e526bc
B O L U W A T I F E <boluwatifeajose@gmail.com>
2026-03-28 17:03:25 +0100

    feat: add staff schema

--- lib/schema.ts
+++ lib/schema.ts
@@ -98,3 +98,21 @@ export const addTeacherSchema = z
     message: "Passwords do not match",
     path: ["confirmPassword"],
   });
+
+export const addStaffSchema = z
+  .object({
+    firstName: z.string().min(1, { message: "First name is required" }),
+    lastName: z.string().min(1, { message: "Last name is required" }),
+    gender: z.enum(genderOptions.map((option) => option.value)),
+    identifier: z.string().min(1, { message: "Identifier is required" }),
+    qualification: z.string().min(1, { message: "Qualification is required" }),
+    password: z.string().min(1, { message: "Password is required" }),
+    confirmPassword: z
+      .string()
+      .min(1, { message: "Confirm password is required" }),
+    enforceChangePassword: z.boolean().default(false),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
